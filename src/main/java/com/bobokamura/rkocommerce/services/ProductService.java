75b9e76243b50ceb412cffb50abd40150b9dcd33
bobokamura <roberto.okamura@cotefacil.com>
2022-12-16 10:50:02 -0300

    POST new product

--- src/main/java/com/bobokamura/rkocommerce/services/ProductService.java
+++ src/main/java/com/bobokamura/rkocommerce/services/ProductService.java
@@ -1,11 +1,13 @@
 package com.bobokamura.rkocommerce.services;
 
 import com.bobokamura.rkocommerce.dtos.ProductDTO;
+import com.bobokamura.rkocommerce.entities.Category;
 import com.bobokamura.rkocommerce.entities.Product;
 import com.bobokamura.rkocommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,5 +27,20 @@ public class ProductService {
     public Page<ProductDTO> findAllProducts(Pageable pageable) {
         Page<Product> list = repository.findAll(pageable);
         return list.map(ProductDTO::new);
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+        CopyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+    private static void CopyDtoToEntity(ProductDTO dto, Product entity) {
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
     }
 }
