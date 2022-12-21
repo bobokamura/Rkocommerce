64942b3591b1434b6fba7b448123561421d50697
bobokamura <roberto.okamura@cotefacil.com>
2022-12-21 12:11:16 -0300

    feat: UserRepository

---
+++ src/main/java/com/bobokamura/rkocommerce/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.bobokamura.rkocommerce.repositories;
+
+import com.bobokamura.rkocommerce.entities.Product;
+import com.bobokamura.rkocommerce.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
