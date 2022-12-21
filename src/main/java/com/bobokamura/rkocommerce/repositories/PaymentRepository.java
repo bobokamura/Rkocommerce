f1e3a52edba021b209e6b2a749fb1f8fac6a43a6
bobokamura <roberto.okamura@cotefacil.com>
2022-12-21 12:08:58 -0300

    chore: @Repository

--- src/main/java/com/bobokamura/rkocommerce/repositories/PaymentRepository.java
+++ src/main/java/com/bobokamura/rkocommerce/repositories/PaymentRepository.java
@@ -3,6 +3,8 @@ package com.bobokamura.rkocommerce.repositories;
 import com.bobokamura.rkocommerce.entities.Payment;
 import com.bobokamura.rkocommerce.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
