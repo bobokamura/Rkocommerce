ff86e62da85c2580b8cc3435a1ed8140e6900847
bobokamura <roberto.okamura@cotefacil.com>
2022-12-21 15:38:14 -0300

    Delete UserRepository feature/007

--- src/main/java/com/bobokamura/rkocommerce/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bobokamura.rkocommerce.repositories;
-
-import com.bobokamura.rkocommerce.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
